86a224f9a3c51a25c8e6939287b9f692369fc78e
Alex Fujiama <alex.fujiama@gmail.com>
2020-06-21 14:52:34 -0300

    Conectando o banco de dados com o backend

--- backend/src/controllers/ProductController.js
+++ backend/src/controllers/ProductController.js
@@ -1,9 +1,37 @@
 const crypto = require('crypto');
+const connection = require('../database/connection');
+const fileSystem = require('fs');
 
 module.exports = {
-    async newProduct(require, response) {
+    async index(request, response) {
+        const products = await connection('products').select('*');
+    
+        return response.json(products);
+    },
+
+    async productInfo(request, response) {
+        const { id } = request.body;
+
+        const info = await connection('products')
+        .where('id', id)
+        .select('*')
+        .first();
+
+        return response.json(info)
+    },
+    
+    async newProduct(request, response) {
+        const { title, value, stock } = request.body;
+
         const id = crypto.randomBytes(4).toString('HEX');
+
+        await connection('products').insert({
+            id,
+            title,
+            value,
+            stock
+        });
 
         return response.json({ id });
     }
-}
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -6,6 +6,8 @@ const routes = express();
 const ProductController = require('./controllers/ProductController');
 
 
-routes.post('/newProduct', ProductController.newProduct);
+routes.get('/products', ProductController.index);
+routes.post('/products/new', ProductController.newProduct);
+routes.post('/products/info', ProductController.productInfo);
 
 module.exports = routes;
