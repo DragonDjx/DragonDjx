06e576123ed1f26501ecf7e92c53e75ac59d0fb6
Alex Fujiama <alex.fujiama@gmail.com>
2020-06-25 18:03:23 -0300

    Adicionando função para upload de imagem

---
+++ backend/src/controllers/ImageController.js
@@ -0,0 +1,42 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    async newImage(request, response) {
+        const product_id = request.body.id;
+        const { originalname: name, size, key } = request.file;
+
+        const url = `http://localhost:3333/files/${key}`
+
+        await connection('images').insert({
+            name,
+            size,
+            key,
+            url,
+
+            product_id
+        });
+
+        return response.json({  })
+    },
+
+    async firstImage(request, response) {
+        const { id: product_id } = request.body;
+
+        const image = await connection('images')
+            .where('product_id', product_id)
+            .first()
+            .select('*');
+
+        return response.json(image);
+    },
+
+    async allImages(request, response) {
+        const { id: product_id } = request.body;
+
+        const images = await connection('images')
+            .where('product_id', product_id)
+            .select('*');
+
+        return response.json(images);
+    }
+};
