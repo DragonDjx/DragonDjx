717333e3c7746eb8766abccf87f8a7e38fe20449
The-Livrodjx <45701263+The-Livrodjx@users.noreply.github.com>
2020-08-23 12:08:38 -0300

    Feature: Form Validation

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -4,13 +4,58 @@ import { Link } from 'react-router-dom';
 import './styles.css'
 
 function Login() {
+    function validaForm (frm) {
+
+        frm.preventDefault()
+        /*
+        o parâmetro frm desta função significa: this.form,
+        pois a chamada da função - validaForm(this) foi
+        definida na tag form.
+
+        The param frm means: this.form,
+        because the function call - validaForm(this) was 
+        defined in the tag form
+        */
+
+        //Verifica se o campo nome foi preenchido e
+        //contém no mínimo três caracteres.
+
+
+        //o campo e-mail precisa de conter: "@", "." e não pode estar vazio
+        //the input e-mail must have: "@", "." and not empty
+        if(frm.target.email.value.indexOf("@") === -1 ||
+        frm.target.email.value.indexOf(".") === -1 ||
+        frm.target.email.value === "" ||
+        frm.target.email.value === null) {
+            alert("Por favor, indique um e-mail válido.");
+            frm.target.email.focus();
+            return false;
+        }
+        
+        if(frm.target.psw.value === "" || frm.target.psw.value === null || frm.target.psw.value.lenght < 3) {
+            //É mostrado um alerta, caso o campo esteja vazio.
+            // Show an alert, if the input is empty
+            alert("Por favor, indique a sua senha.");
+            //Foi definido um focus no campo.
+            //Was defined a focus on input
+            frm.target.psw.focus();
+            //o form não é enviado.
+            //The form isn´t sent
+            return false;
+        }
+
+
+        else {
+            alert('teste')
+        }
+    }
 
     return(
         <div className="login-page">
             <div className="content-login">
 
                 <div className="login-form">
-                    <form>
+                    <form onSubmit={e => validaForm(e)}>
                         <h1>Login</h1>
                         <div className="form-group">
                             <input type="email" name="email" placeholder="E-mail" />
